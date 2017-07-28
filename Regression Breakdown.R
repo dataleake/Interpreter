4dba73d784ff9fa6bf797214690e33bac17b0503
dataleake <volcoman234@aim.com>
2017-07-28 10:59:51 -0400

    Add starter code for Regression

---
+++ Regression Breakdown.R
@@ -0,0 +1,75 @@
+## Big picture, have a function that can call other functions for interpretation based on "type"
+# Regression Interpretation
+#
+# Interpreter <- function(type = c("lm", "mlm", "ttest"))) {
+#     .Call(function of type)
+#     return(description of output for type)
+#}
+#
+#
+#
+library(ISLR)
+data(Auto)
+
+Regression <- function(dataset = Auto, outcome = 1, predictor = 3, pred2 = NULL) {
+  
+  if(is.null(pred2)) {
+    
+      fit <- lm(dataset[,outcome] ~ dataset[,predictor])
+      
+        } else {
+          
+             fit <- lm(dataset[,outcome] ~ dataset[,predictor] + dataset[,pred2])
+        }
+  
+  stats <- summary.lm(fit, correlation = TRUE)
+  
+  
+  selection <- function() {
+   
+     output <- data.frame(ID = 1:length(stats), Statistics = names(stats))
+    print(output)
+    
+        x <- readline("Which output would you like to see? ")
+        x <- as.numeric(x)
+  
+
+  if(is.numeric(x) & x <= length(stats)) { 
+        
+        print(noquote(paste("You selected number", x, "which is", output[x,2])))
+        message(noquote(paste("Retrieving", paste0(output[x, 2], "..."))))
+        return(stats[[x]])
+        
+        } else {
+
+       message("Please pick from the ID numbers shown")
+          x <- NULL
+          Regression()
+          
+        }
+  }
+  print(selection())
+  
+  y <- 1
+  
+  while(y != "y" & y != "n") {
+    
+    y <- readline("Would you like to see another? [y/n]")
+    
+  if(y == "Y" | y == "y") {
+      result <- selection()
+         print(result)
+         
+        } else if(y == "N" | y == "n") {
+            print("Ok then, thanks for learning")
+              break
+          
+            }  else {
+                print("I do not understand")
+                  next
+    }
+  }
+}
+
+Regression()
+Regression(dataset = Boston, outcome = "medv", "lstat", "age")
